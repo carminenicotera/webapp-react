27c5aedab3df69724c7dd4e2e87c1225d8f7e461
carminenicotera <carmine.91.n@hotmail.it>
2026-04-21 16:23:48 +0200

    milestone 2 complete

---
+++ src/layouts/DefaultLayout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+
+export default function DefaultLayout() {
+
+
+  return (
+    <>
+      <header>header</header>
+      <main>
+        <Outlet />
+      </main>
+      <footer>footer</footer>
+    </>
+  )
+}
